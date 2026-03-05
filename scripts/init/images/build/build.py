500028640d2a93bef3f41e10fe556e77f84cf0ff
Gramovich Alexander <gramovich.alex@outlook.com>
2026-03-05 12:22:34 +0300

    get formatted_tfvars in python approach for scripts/init/images

--- scripts/init/images/build/build.py
+++ scripts/init/images/build/build.py
@@ -1,22 +1,9 @@
-import subprocess
 import os
 from libs.py.helpers import run_command
-
-WORKSPACE_FOLDER = os.getenv("BUILD_WORKSPACE_DIRECTORY")
-
-
-def process_results(result):
-    if result.returncode == 0:
-        return result.stdout
-    else:
-        error = f"Bazel query failed with return code: {result.returncode}\n"
-        error += "Error:\n"
-        error += result.stderr
-
-        raise BaseException(error)
+from libs.py.settings import bazel_settings
 
 
-def build_images():
+def build_images() -> None:
     """
     Build and push custom images to container registries
 
@@ -24,12 +11,12 @@ def build_images():
     1. by executing bazel command and query all targets starting with `push_` in all subpackages of deps/images
     2. executing all those target via `bazel run`
     """
-    os.chdir(WORKSPACE_FOLDER)
+    os.chdir(bazel_settings.workspace)
     query = ["bazel", "query", 'attr(name, "^push_.*$", "//deps/images/...")']
-    result = subprocess.run(query, capture_output=True, text=True)
-    output = process_results(result)
-    output = output.strip("\n")
-    for target in output.split("\n"):
+    return_code, stderr, stdout = run_command(
+        query, print_stdout=True, print_stderr=True
+    )
+    for target in stdout:
         command = ["bazel", "run", target]
         run_command(command)
 
