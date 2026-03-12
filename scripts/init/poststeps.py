19528cb7bfefb30b2ce03b3259d02620abdcc23d
Gramovich Alexander <gramovich.alex@outlook.com>
2026-03-13 00:53:04 +0300

    libs/py/tf/tfvars env_key function

--- scripts/init/poststeps.py
+++ scripts/init/poststeps.py
@@ -5,7 +5,7 @@ from libs.py.settings import bazel_settings
 
 def clean() -> None:
     os.chdir(bazel_settings.workspace)
-    shutil.rmtree('tests/e2e')
+    shutil.rmtree("tests/e2e")
 
 
 if __name__ == "__main__":
