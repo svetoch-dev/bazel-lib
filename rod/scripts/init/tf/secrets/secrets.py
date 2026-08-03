2fa6445ac3abd213f82fdbb7d166d10e7fa30a1e
Gramovich Alexander <gramovich.alex@outlook.com>
2026-08-03 19:22:22 +0300

    rod/scripts/init/tf/secrets/secrets.py: import_secrets only if tf target exists

--- rod/scripts/init/tf/secrets/secrets.py
+++ rod/scripts/init/tf/secrets/secrets.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from rod.libs.py.helpers import run_command
+from rod.libs.py.settings import bazel_settings
 from rod.libs.py.tf.tfvars import tfvars
 from rod.libs.py.tf.secrets import import_secrets
 from rod.libs.py.utils.logger import CliLogger
@@ -10,6 +12,14 @@ def secrets() -> None:
     logger = CliLogger("rod.scripts.init.tf.secrets.secrets")
 
     for env_name, env_obj in tf_vars.envs.items():
+        secrets_package = f"//{bazel_settings.tf_env_dir}/{env_name}/secrets"
+        query = ["bazel", "query", f'attr(name, "^tf$", "{secrets_package}")']
+        exit_code, stderr, _ = run_command(query, print_stdout=False)
+        if exit_code != 0:
+           logger.info(f"{secret_package}:tf target not found")
+           continue
+
+
         imported = import_secrets(env_name, env_obj.import_secrets)
         if not imported:
             logger.error(f"Import for secrets of {env_name} has failed")
