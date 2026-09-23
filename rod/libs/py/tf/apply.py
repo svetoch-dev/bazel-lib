4ae4b178fa0f3976120690e04fd118cdb63466fd
dmitry231187 <leshiy74@gmail.com>
2026-09-23 07:47:54 +0000

    correct error description

--- rod/libs/py/tf/apply.py
+++ rod/libs/py/tf/apply.py
@@ -76,6 +76,10 @@ def apply_env(
 
         return_code, stderr, stdout = run_command(command)
         if return_code != 0:
+            logger.error(
+                f"Failed to apply target {target} for {env} "
+                f"(exit code {return_code})"
+            )
             return False
 
     return True
